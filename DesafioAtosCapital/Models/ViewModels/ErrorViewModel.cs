905fc06d1cba2bdca15bfff1324514975cc86e06
brunoaragao <bruno20aragao@gmail.com>
2020-03-05 08:45:18 -0300

    Inicio do projeto

---
+++ DesafioAtosCapital/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesafioAtosCapital.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
